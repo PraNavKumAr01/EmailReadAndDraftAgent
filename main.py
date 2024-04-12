64cda2171c7b7a0016990d33379b2f794d930a75
Pranav Kumar <96071514+PraNavKumAr01@users.noreply.github.com>
2024-04-12 12:35:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,24 @@ app = FastAPI()
 class EmailInput(BaseModel):
     emailID: str
 
-@app.post("/invoke_workflow/")
+@app.post("/invoke_workflow")
 def invoke_workflow(email_input: EmailInput):
     try:
         mail = email_input.emailID
         workflow_app = WorkFlow(emailID=mail).app
         workflow_app.invoke({})
 
+        return {"message": "Workflow invoked successfully!"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to invoke workflow: {str(e)}")
+
+@app.get("/get_drafts")
+def get_drafts():
+    try:
         # Read the contents of output.txt
         with open('output.txt', 'r') as file:
             output_text = file.read()
 
-        return {"message": "Workflow invoked successfully!", "output_text": output_text}
+        return {"output_text": output_text}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to invoke workflow: {str(e)}")
